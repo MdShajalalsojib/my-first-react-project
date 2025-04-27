7937640232339c4a973e26b49b3d09d65e3e3e81
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-04-27 17:11:38 +0600

    body de

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar/Navbar'
+import Blogs from './components/Navbar/Blogs/Blogs'
 import './App.css'
 
 function App() {
@@ -8,6 +9,20 @@ function App() {
     <>
        
        <Navbar></Navbar>
+        
+       <div className="main-container flex text-center ">
+          <div className="left-container w-[70%]">
+              
+             <Blogs></Blogs>
+
+          </div>
+          <div className="right-container w-[30%] bg-gray-50 ">
+            <h1>Reading time : 0</h1>
+            <h2>Bookmakred count : 0</h2>
+
+          </div>
+
+       </div>
        
     </>
   )
